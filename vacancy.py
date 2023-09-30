67e6461d7998a54ee40911902896d4389bf73edf
Aleksandr <ribakov.alex2013@yandex.ru>
2023-09-30 15:19:59 +0300

    класс для работы с вакансиями, функциями для сравнения и вывода

--- vacancy.py
+++ vacancy.py
@@ -1,34 +1,54 @@
 class Vacancy:
     def __init__(self, title, salary, description, url):
         self.title = title
-        self.salary = salary
+        self.salary = self.get_salary(salary)
         self.description = description
         self.url = url
 
     def __repr__(self) -> str:
         return (f'\n\nВакансия: {self.title}\n'
-                f'Зарплата: {self.salary}\n'
+                f'Зарплата: {self.format_salary()}\n'
                 f'Требования: {self.description}\n'
                 f'Ссылка: {self.url}')
 
-    @staticmethod
-    def vacancy_filter(vacancy: dict) -> dict:
-        """Проверка зарплаты(указана, не указана) и """
-        filtered_vacancy = {'title': vacancy['name'],
-                            'salary': vacancy['salary'],
-                            'description': vacancy['snippet']['requirement'],
-                            'url': vacancy['url']}
-        if filtered_vacancy['salary'] is None:
-            filtered_vacancy['salary'] = 'Не указана'
-        else:
-            sal_from = filtered_vacancy['salary']['from']
-            sal_to = filtered_vacancy['salary']['to']
-            currency = filtered_vacancy['salary']['currency']
-            if sal_to is None:
-                filtered_vacancy['salary'] = f'{sal_from} {currency}'
-            elif sal_from is None:
-                filtered_vacancy['salary'] = f'{sal_to} {currency}'
-            else:
-                filtered_vacancy['salary'] = f'{sal_from} - {sal_to} {currency}'
-        return filtered_vacancy
+    def __lt__(self, other):
+        """
+        :param other: сравнение заработной платы
+        :return: True or False
+        """
+        return (self.salary['from'], self.salary['to']) < (other.salary['from'], other.salary['to'])
 
+    def __gt__(self, other):
+        """
+        :param other: сравнение заработной платы
+        :return: True or False
+        """
+        return (self.salary['from'], self.salary['to']) > (other.salary['from'], other.salary['to'])
+
+    @staticmethod
+    def get_salary(salary):
+        """
+        :param salary:
+        :return: словарь salary {'from': ... , 'to': ...}
+        """
+        if salary is None:
+            salary = {
+                'from': 0,
+                'to': 0
+            }
+            return salary
+        else:
+            salary_from = salary['from']
+            salary_to = salary['to']
+            if salary_to is None:
+                return {'to': 0, 'from': salary_from}
+            elif salary_from is None:
+                return {'to': salary_to, 'from': 0}
+            else:
+                return salary
+
+    def format_salary(self):
+        """
+        :return: Формат вывода зарплаты
+        """
+        return f'{self.salary["from"]} - {self.salary["to"]}'
